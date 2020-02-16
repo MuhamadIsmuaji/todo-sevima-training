ae3ad4ca346a5fc0d28aa2091e96747858ff173c
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2020-02-16 17:54:03 +0700

    Updates

---
+++ precache-manifest.45371f985342acc6ddaf16109311df09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20043e4f2173db2a5a4fdf776060284",
+    "url": "/todo-sevima-training/index.html"
+  },
+  {
+    "revision": "b6e5b95c8e20374abb6c",
+    "url": "/todo-sevima-training/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "149ce78484d69b5777f7",
+    "url": "/todo-sevima-training/static/css/main.e17ac13f.chunk.css"
+  },
+  {
+    "revision": "b6e5b95c8e20374abb6c",
+    "url": "/todo-sevima-training/static/js/2.1185c8be.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-sevima-training/static/js/2.1185c8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149ce78484d69b5777f7",
+    "url": "/todo-sevima-training/static/js/main.c9f0fc13.chunk.js"
+  },
+  {
+    "revision": "228f44294c5ba3ab30ec",
+    "url": "/todo-sevima-training/static/js/runtime-main.7f7dde68.js"
+  }
+]);
